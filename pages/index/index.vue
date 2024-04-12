64e5d90dd72780c214ec4fc15ad0cef8a5cc7802
lisy999 <2574561698@qq.com>
2024-04-12 17:15:33 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  {{name}}
+  {{car.name}}
+  {{car.price}}
+</div>
+</template>
+
+<script setup lang='ts'>
+import { ref, reactive } from 'vue'
+const  name = ref("李四")
+const car = reactive({name:"丰田",price:20})
+</script>
+
+<style scoped>
+
+</style>

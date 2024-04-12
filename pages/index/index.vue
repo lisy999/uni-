f5659ff3e303d883b20bf50a0fce56a3bddc9d25
lisy999 <2574561698@qq.com>
2024-04-12 17:29:13 +0800

    更改了一些方法

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -3,13 +3,18 @@
   {{name}}
   {{car.name}}
   {{car.price}}
+  <button @click="changeName">改变名字</button>
 </div>
 </template>
 
 <script setup lang='ts'>
 import { ref, reactive } from 'vue'
-const  name = ref("李四")
+const name = ref("李四")
 const car = reactive({name:"丰田",price:20})
+	
+const changeName = ()=>{
+	name.value= "王五"
+}
 </script>
 
 <style scoped>
